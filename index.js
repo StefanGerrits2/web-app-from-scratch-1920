71c5e18947f5cd8acc1cc47e82cc7b716564f88e
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-02-03 20:03:05 +0100

    create cards, add images

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-fetch('https://cors-anywhere.herokuapp.com/https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6')
+fetch('https://cors-anywhere.herokuapp.com/https://api.punkapi.com/v2/beers?page=1&per_page=80')
     .then((response) => {
         return response.json();
     })
-    .then((cleanData) => {
-        cleanData.map(data => displayNames(data))
-        console.log(cleanData)
+    .then((rawData) => {
+        rawData.map(rawData => displayData(rawData))
+        console.log(rawData)
     })
 
-function displayNames(data) {
-    var myH2 = document.createElement("h2");
-    myH2.innerHTML = data.name
-    document.body.appendChild(myH2);
+const cardsDiv = document.getElementById("cardsDiv")
+    document.body.appendChild(cardsDiv);
 
-    var myImg = document.createElement("p");
-    myImg.innerHTML = data.tagline
-    document.body.appendChild(myImg);
+function displayData(data) {
+    // Create an article for each beer item
+    let cardDiv = document.createElement("article")
+    cardsDiv.appendChild(cardDiv);
+
+    // Append beer name
+    const myH2 = document.createElement("h2");
+    myH2.textContent = data.name
+    cardDiv.appendChild(myH2);
+
+    // Append image
+    const image =  document.createElement("div");   
+    image.innerHTML += '<img src="' + data['image_url'] + '" />';
+    cardDiv.appendChild(image);
+
+    // Append tagline
+    const tagLine = document.createElement("p");
+    tagLine.textContent = data.tagline
+    cardDiv.appendChild(tagLine);
 }
