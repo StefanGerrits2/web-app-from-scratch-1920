6c83311a31eed2fdf2d50ab5f4c9593903790ada
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-02-10 11:51:06 +0100

    clean code a bit

--- docs/modules/renderCard.js
+++ docs/modules/renderCard.js
@@ -4,16 +4,16 @@ export default function renderCard(data) {
         h2 = document.createElement('h2'),
         p = document.createElement('p'),
         img = document.createElement('img');
-                    
-    card.setAttribute('class', 'card')
+    
+    card.setAttribute('class', 'card');
 
     h2.textContent = data.name;
     p.textContent = data.tagline;
     img.src = data.image_url;
 
-    card.appendChild(h2)
-    card.appendChild(img)
-    card.appendChild(p)
+    card.appendChild(h2);
+    card.appendChild(img);
+    card.appendChild(p);
 
     return card;
 }
--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 import { Fetcher } from './docs/modules/fetch.js';
-import renderCard from './docs/modules/renderCard.js'
+import renderCard from './docs/modules/renderCard.js';
 
 // Set standard query values
-let topic = 'beers'
-let page = '1'
-let amount = '12'
+let topic = 'beers';
+let page = '1';
+let amount = '12';
 
-// Set standard values
-const cardsContainer = document.getElementById('cardsDiv')
+// Run main app
+loadApp(topic, page, amount);
 
 // Main function
 function loadApp(topic, page, amount) {
     // Set url values
-    const corsUrl = "https://cors-anywhere.herokuapp.com/" ;
-    const apiUrl = "https://api.punkapi.com/v2/";
-    const url = `${corsUrl}${apiUrl}${topic}?page=${page}&per_page=${amount}`;
+    const apiUrl = 'https://api.punkapi.com/v2/';
+    const url = `${apiUrl}${topic}?page=${page}&per_page=${amount}`;
 
     return Fetcher.get(url)
         .then(data => {
             console.log(data)
             data.forEach(data => {  
-                cardsContainer.appendChild(renderCard(data))
-            })
-        })
-}
+                document.getElementById('cardsDiv').appendChild(renderCard(data));
+            });
+        });
+};
 
 // Pagination buttons
-const loadButton = document.getElementById('nextPage')
-loadButton.addEventListener('click', renderMoreCards)
+document.getElementById('nextPage').addEventListener('click', renderMoreCards);
 
 function renderMoreCards() {
     // Load more cards
-    page++
-    loadApp(topic, page, amount)
-}
-
-// Run main app
-loadApp(topic, page, amount);
+    page++;
+    loadApp(topic, page, amount);
+};
