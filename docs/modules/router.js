2c46904b6a87c44b7ee2c57842bd9b8195ec6e65
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-02-18 20:28:33 +0100

    fixed undefined bug

--- docs/modules/router.js
+++ docs/modules/router.js
@@ -4,11 +4,11 @@ import renderDetailPage from './renderDetailCard.js';
 export default function router(data) {
     routie({
         '/': 
-            document.querySelector('.overview__container').append(renderCard(data)),        
+            renderCard(data),        
         ':id': (id) => data.beers.forEach(item => {
             if (item.id == id) {
                 console.log(item);
-                document.querySelector('.details__container').append(renderDetailPage(item));
+                renderDetailPage(item);
             }
         })
     });
